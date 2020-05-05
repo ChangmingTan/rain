81cbcb4424f33573a83166c6b3e283e4ed5d70f2
Changming Tan <ctan9@mail.greenriver.edu>
2020-05-05 05:00:13 -0400

    added views and index.php

--- index.php
+++ index.php
@@ -5,15 +5,18 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 //Require the autoload file
-require_once('vendor/autoload.php');
+require_once("vendor/autoload.php");
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
 
-//Define a default route
+//Default route
 $f3->route('GET /', function () {
-    echo '<h1>It is raining today!</h1>';
+    //echo '<h1>It is raining today</h1>';
+
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
-//Run fat free
+//Run F3
 $f3->run();
